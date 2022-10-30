b5ff2a6cf74393b204a00cbcecedc891dc12d9a3
Ricky <46491224+Rickaphile@users.noreply.github.com>
2022-10-30 12:32:17 -0700

    Add files via upload

---
+++ Computing GC Content/1.py
@@ -0,0 +1,28 @@
+f = open('ROSALIND/Computing GC Content/rosalind_gc.txt')
+
+lines = f.readlines()
+
+# Collect sequence data
+seqs = {}
+for l in lines:
+    if l[0] == '>':
+        seq = l
+        seqs[seq] = ''
+    else:
+        l = l.strip()
+        seqs[seq] = seqs[seq] + l
+
+# Find max GC, l:labels, s:sequences
+label = ''
+max = float(0)
+for l in seqs:
+    GC = 0
+    for t in seqs[l]:
+        if t == 'G' or t == 'C':
+            GC += 1
+    val = GC / len(seqs[l])
+    if max < val:
+        max = val
+        label = l
+
+print(label, max)
