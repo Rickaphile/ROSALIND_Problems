28254a3c07b2ffa4de49d780916f4d7fa14a86b7
Ricky <46491224+Rickaphile@users.noreply.github.com>
2022-10-30 12:29:16 -0700

    Add files via upload

---
+++ Counting DNA Nucleotides/1.py
@@ -0,0 +1,20 @@
+f = open("rosalind_dna.txt")
+
+DNA = f.read()
+
+A = 0
+G = 0
+C = 0
+T = 0
+
+for dna in DNA:
+    if dna == 'A':
+        A += 1
+    if dna == 'G':
+        G += 1
+    if dna == 'C':
+        C += 1
+    if dna == 'T':
+        T += 1
+
+print(A, C, G, T)
