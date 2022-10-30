df646f9417259ec3d3d8e3fd66d6043c265e72fe
Ricky <46491224+Rickaphile@users.noreply.github.com>
2022-10-30 12:31:40 -0700

    Add files via upload

---
+++ Rabbits and Recurrence Relations/1.py
@@ -0,0 +1,13 @@
+f = open('ROSALIND/Rabbits and Recurrence Relations/rosalind_fib.txt')
+
+metrics = f.read()
+n, k = int(metrics.split(' ')[0]), int(metrics.split(' ')[1])
+
+# 1, 1, 1+k, (1+k)+k, (1+k+k)+k(1+k)
+def wabbits(n, k):
+    if n == 0: return 0
+    if n == 1: return 1
+    else:
+        return wabbits(n-1, k) + k*wabbits(n-2, k)
+
+print(wabbits(n, k))
